ad51c45b0751b37614befd3d344e9f29cc3a514a
reefberrypi <reefberrypi@lynq.nl>
2015-03-05 21:39:59 +0100

    Added cron app

--- ReefberryPi/settings.py
+++ ReefberryPi/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lightcontrol'
+    'lightcontrol',
+    'cron'
 )
 
 MIDDLEWARE_CLASSES = (
