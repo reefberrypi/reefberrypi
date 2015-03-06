222d197b2d2de726b31ef1d81facd12cf54d35d4
reefberrypi <reefberrypi@lynq.nl>
2015-03-06 22:59:39 +0100

    Updated settings for deployment

--- ReefberryPi/settings.py
+++ ReefberryPi/settings.py
@@ -82,4 +82,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+MEDIA_ROOT = '/home/reefberrypi/reefberrypi/media'
+MEDIA_URL = '/media/'
+STATIC_ROOT = '/home/reefberrypi/reefberrypi/static'
 STATIC_URL = '/static/'
